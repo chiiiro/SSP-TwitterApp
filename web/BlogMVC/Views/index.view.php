f86faa99b62ea4e632e0fb6b6c9ae0a79ca8ab68
chiiiro <ivan.ciric@sofascore.com>
2015-11-04 12:20:20 +0100

    Blog

--- web/BlogMVC/Views/index.view.php
+++ web/BlogMVC/Views/index.view.php
@@ -2,13 +2,20 @@
 require_once(VIEW_PATH . 'header.php');
 ?>
 
-    <body>
+    <body background="clouds.jpg" style="background-size: cover">
     <div class="container">
         <h1></h1>
         <h1></h1>
 
 
-    <div class="jumbotron">
+    <div style="background: transparent" class="jumbotron">
+
+        <ul class="nav nav-tabs">
+            <li role="presentation" class="active"><a href="index.php">Home</a></li>
+            <li role="presentation"><a href="Controllers/login.php">Login</a></li>
+            <li role="presentation"><a href="Controllers/register.php">Register</a></li>
+        </ul>
+
         <h1>Home page</h1>
         <p>This is a simple blog created for learning purposes. New user can register, login and then add, update
             or delete posts. Unregistered users can only view posts stored in database.
@@ -35,13 +42,6 @@ require_once(VIEW_PATH . 'header.php');
         ?>
 
     </div>
-
-
-
-    <form class="form-horizontal" role="form" action="" method="post">
-        <input type="submit" class="btn btn-default" name="login" id="login" value="Login">
-        <input type="submit" class="btn btn-default" name="register" id="register" value="Register">
-    </form>
 
 <?php
 require_once(VIEW_PATH . 'footer.php');
